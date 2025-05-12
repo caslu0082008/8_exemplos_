648daf2cffde4551ebc92cbf30949f37419d81a2
caslu0082008 <lucasaguiarbarbosaaguiar@gmail.com>
2025-05-12 20:11:55 -0300

    terminando os exemplos

---
+++ main.js
@@ -0,0 +1,91 @@
+//Exemplo 1
+
+function mensagem(){
+    alert("Seja bem-vindo(a)!")
+    console.log("Seja bem-vindo(a)!")
+}
+mensagem()
+
+/*
+//Exemplo 2 - função com parâmetros
+
+function somar(numero1, numero2){
+    alert(numero1 + numero2)
+    console.log(numero1 + numero2)
+}
+
+let n1 = Number(prompt("Digite um numero"))
+let n2 = Number(prompt("Digite outro numero"))
+
+somar(n1, n2)
+somar(11, 8)
+somar("14", 18)
+*/
+
+/*
+//Exemplo 3 - parâmetros de tipos diferentes
+
+function mensagemPessoal (nome, cargo, anosTrabalhados) {
+    alert(`Seja bem-vindo(a) ${cargo} ${nome}. Estamos felizes por ter voce trabalhando conosco há ${anosTrabalhados} anos.`)
+}
+
+mensagemPessoal ("Lucas", "Gerente", 18)
+mensagemPessoal ("Irineu", "Você não sabe, nem eu", 58)
+*/
+/*
+//Exemplo 4 - return nas funções
+
+function somarEDevolver (numero1, numero2, numero3) {
+
+    let resultado = numero1 + numero2 + numero3
+    return resultado
+}
+let soma1 = somarEDevolver (18, 11, 12)
+let soma2 = somarEDevolver (30, 40, 50)
+
+alert(soma1)
+alert(soma2)
+*/
+
+
+/*
+
+//EXEMPLO 5 if sem o else
+
+
+function parouImpar (numero) {
+    if (numero % 2 === 0) {
+        return "PAR"
+    }
+    return "ÍMPAR"
+}
+
+let resultado1 = parouImpar(10)
+let resultado2 = parouImpar (15)
+
+alert(`O número 10 é ${resultado1}`)
+alert(`O número 15 é ${resultado2}`)
+*/
+
+/*
+//EXEMPLO 6 funções anonimas
+
+document.querySelector("button").addEventListener("click", function() {
+    alert("Clicou no botão")
+})
+*/
+
+/*
+//EXEMPLO 7 arrow function
+
+document.querySelector("button").addEventListener("click", () => { 
+    alert("Clicou no botão")
+})
+*/
+
+/*
+
+EXEMPLO 8 arrow functions com variáveis
+
+let somar = (valor1, valor2) => valor1 + valor2
+let resultadoSoma = somar (20, 30);
